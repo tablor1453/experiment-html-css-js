9fca704c80fdf4b6757792102183cc40b5a64174
tablor1453 <rijalahmadt@gmail.com>
2024-01-14 09:09:55 +0700

    Added glowing effect

---
+++ main.js
@@ -0,0 +1,15 @@
+(function setGlowEffectRx() {
+    const glowEffects = document.querySelectorAll('.glow-effect');
+
+    glowEffects.forEach(glowEffect => {
+        const glowLines = glowEffect.querySelectorAll('rect');
+        const rx = getComputedStyle(glowEffect).borderRadius;
+
+        glowLines.forEach(line => {
+            line.setAttribute('rx', rx);
+        });
+        
+        console.log(rx);
+    })
+
+})();
